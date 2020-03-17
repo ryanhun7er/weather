867d62c20a5fcd8a79ca5f3d214f5021f91e982b
Ryan Hunter <ryanhwilkinson@icloud.com>
2020-03-17 12:48:20 -0500

    update add 5 day forecast

--- assets/script.js
+++ assets/script.js
@@ -18,17 +18,13 @@ $(document).ready(function () {
 
 })
 
-
-
-function gatherWeather(city) {
-
-// variables for api
-
+var city = "Chicago"
 var APIkey = 'eab4186c021254a40cb2caf858df2e69';
 //var city = 'Nashville';
 var queryURL = 'https://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=' + APIkey;
+var queryURL2 = 'https://api.openweathermap.org/data/2.5/forecast?q=' + city + '&appid=' + APIkey;
 
-
+function gatherWeather() {
 
 
 //ajax for calling weather information
@@ -41,23 +37,72 @@ $.ajax({
 
     console.log(response);
 
-    var cityName = $("<div>").text("City: " + response.name);
-    var temp = $("<div>").text("Current Temperature: " + response.main.temp);
-    var humidity = $("<div>").text("Humidity: " + response.main.humidity);
-    var wind = $("<div>").text("Wind Speed: " + response.wind.speed);
+    var cityName = $("<h4>").text(response.name);
+    var temp = $("<div>").text("Current Temperature: " + ((Math.floor(response.main.temp -273.15) * 1.8) + 32 + "\xB0F"));
+    var high = $("<div>").text("Expected High Temp: " + ((Math.floor(response.main.temp_max -273.15) * 1.8) + 32 + "\xB0F"));
+    var low = $("<div>").text("Expected Low Temp: " + ((Math.floor(response.main.temp_min -273.15) * 1.8) + 32 + "\xB0F"));
+    var humidity = $("<div>").text("Humidity: " + response.main.humidity + " " + "%");
+    var wind = $("<div>").text("Wind Speed: " + response.wind.speed + " " + "mph");
 
     $("#city-weather").empty();
-    $("#city-weather").append(cityName, temp, humidity, wind);
+    $("#city-weather").append(cityName, temp, high, low, humidity, wind);
 
 
   })
 
 }
+
+function fiveDay() {
+ 
+
+    //ajax for calling 5 day forecast
+
+    $.ajax({
+        url: queryURL2,
+        dataType: 'json',
+        method: "GET",
+        data: {
+            q: city,
+            units: 'imperial',
+            cnt: "5"
+        },
+
+
+      success: function(data) {
+        console.log('Data:', data)
+        var forecast = "";
+        
+
+        forecast += "<h3>" + data.city.name + "</h3>";
+        $.each(data.list, function(dt_txt, val) {
+            var tempH = Math.floor(val.main.temp_max);
+            var tempL = Math.floor(val.main.temp_min);
+            forecast += "<p>";
+            forecast += "<b>" + val.dt_txt + "</b>: ";
+            forecast += "High: " + tempH + "&degF";
+            forecast += " " + "Low: " + tempL + "&degF";
+            forecast += "<span> | " + val.weather[0].description + "</span>";
+            forecast += "<img src='https://openweathermap.org/img/w/" + val.weather[0].icon + ".png'>";
+            forecast += "</p>";
+        });
+
+        $("#fiveDay").html(forecast);
+
+    }
+    
+    
+})
+}
+
+
+
+
 //event handler for user clicking search button
 $("#search").on("click", function(event) {
     event.preventDefault();
 
     var cityInput = $("#city").val().trim();
+    var forecast = $("#city").val().trim();
 
     var searchNum = $(this).attr("id");
     var city = $(this).siblings(".input").val();
@@ -65,4 +110,5 @@ $("#search").on("click", function(event) {
     localStorage.setItem(searchNum, city);
 
     gatherWeather(cityInput);
+    fiveDay(forecast)
 });
